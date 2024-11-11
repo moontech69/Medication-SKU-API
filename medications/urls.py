0172b1e6379eb38be7a10fa9a1c36f7edd3f9fc0
alex <alex.tech93@outlook.com>
2024-11-11 16:49:32 +0100

    Implement bulk upload endpoint for SKUs

--- medications/urls.py
+++ medications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListCreateMedicationSKUAPIView, RetrieveUpdateDestroyMedicationSKUAPIView
+from .views import ListCreateMedicationSKUAPIView, RetrieveUpdateDestroyMedicationSKUAPIView, BulkUploadMedicationSKUAPIView
 
 urlpatterns = [
     path('', ListCreateMedicationSKUAPIView.as_view(), name='get_post_medication'),
     path('<int:pk>/', RetrieveUpdateDestroyMedicationSKUAPIView.as_view(), name='get_delete_update_medication'),
+    path('bulk-upload/', BulkUploadMedicationSKUAPIView.as_view(), name='bulk_upload_medication'),
 ]
--- medications/views.py
+++ medications/views.py
@@ -1,4 +1,8 @@
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView, CreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.db import transaction
+from django.db.utils import IntegrityError
 from django_filters import rest_framework as filters
 from .models import MedicationSKU
 from .serializers import MedicationSKUSerializer
@@ -17,3 +21,22 @@ class ListCreateMedicationSKUAPIView(ListCreateAPIView):
 class RetrieveUpdateDestroyMedicationSKUAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = MedicationSKUSerializer
     queryset = MedicationSKU.objects.all()
+
+class BulkUploadMedicationSKUAPIView(CreateAPIView):
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        # print('request-data: ', data)
+        serializers = [MedicationSKUSerializer(data=item) for item in data]
+        # print('serializers: ', serializers)
+        if all(serializer.is_valid() for serializer in serializers):
+            try:
+                with transaction.atomic():
+                    movie_objects = [MedicationSKU(**serializer.validated_data) for serializer in serializers]
+                    created_movies = MedicationSKU.objects.bulk_create(movie_objects)
+                return Response(MedicationSKUSerializer(created_movies, many=True).data, status=status.HTTP_201_CREATED)
+            
+            except IntegrityError:
+                return Response({"error": "Unique constraint violation"}, status=status.HTTP_400_BAD_REQUEST)
+        errors = [serializer.errors for serializer in serializers if not serializer.is_valid()]
+        return Response({"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
+    
